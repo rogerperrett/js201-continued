29d5e1e7fe5bae6f1db81219c179f7a8bf59408d
roger perret <rogelio_perrett@hotmail.com>
2018-10-01 23:28:39 -0500

    01-predicate-functions finished

--- exercises/01-predicate-functions.js
+++ exercises/01-predicate-functions.js
@@ -21,23 +21,28 @@
 // isVowel(99) --> false
 // isVowel({e: 'Elephant'}) --> false
 var vowels = {
-    a: true,
-    e: true,
-    i: true,
-    o: true,
+	a: true,
+	e: true,
+	i: true,
+	o: true,
     u: true,
+    A: true,
+	E: true,
+	I: true,
+	O: true,
+	U: true,
 }
 var output = '';
-function isVowel (enterLetter) {
-    if (enterLetter in vowels){
-        output = true
+
+function isVowel(inputLetter) {
+	if (inputLetter in vowels) {
+		output = true
+	} else {
+		output = false;
     }
-    else {
-        output = false;
-    }
-return output
+	return output
 }
-isVowel('a')
+isVowel('A')
 
 
 
@@ -55,8 +60,27 @@ isVowel('a')
 // isOdd(5) --> true
 // isOdd('7') --> false
 // isOdd(3.14) --> false
+var output = '';
 
-
+function isEven (inputValue) {
+	if (inputValue % 2 == 0) {
+        output = true;
+    }
+    else {
+        output = false
+    }
+    return output;
+}
+function isOdd (inputValue) {
+	if (inputValue % 2 == 1 || inputValue % 2 == -1) {
+        output = true;
+    }
+    else {
+        output = false
+    }
+    return output;
+}
+isOdd('-3')
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "isCapitalCity" that takes two arguments: a US state and a city name
@@ -71,3 +95,221 @@ isVowel('a')
 // isCapitalCity('Texas', 'Houston') --> false
 // isCapitalCity('Alaska', 'Juneau') --> true
 // isCapitalCity('Strawberry', 'Mango') --> false
+var capitalCities = [{
+    State: 'Alabama',
+    City: 'Montgomery'
+},
+{
+    State: 'Alaska',
+    City: 'Juneau'
+},
+{
+    State: 'Arizona',
+    City: 'Phoenix'
+},
+{
+    State: 'Arkansas',
+    City: 'Little Rock'
+},
+{
+    State: 'California',
+    City: 'Sacramento'
+},
+{
+    State: 'Colorado',
+    City: 'Denver'
+},
+{
+    State: 'Connecticut',
+    City: 'Hartford'
+},
+{
+    State: 'Delaware',
+    City: 'Dover'
+},
+{
+    State: 'Florida',
+    City: 'Tallahassee'
+},
+{
+    State: 'Georgia',
+    City: 'Atlanta'
+},
+{
+    State: 'Hawaii',
+    City: 'Honolulu'
+},
+{
+    State: 'Idaho',
+    City: 'Boise'
+},
+{
+    State: 'Illinois',
+    City: 'Springfield'
+},
+{
+    State: 'Indiana',
+    City: 'Indianapolis'
+},
+{
+    State: 'Iowa',
+    City: 'Des Moines'
+},
+{
+    State: 'Kansas',
+    City: 'Topeka'
+},
+{
+    State: 'Kentucky',
+    City: 'Frankfort'
+},
+{
+    State: 'Louisiana',
+    City: 'Baton Rouge'
+},
+{
+    State: 'Maine',
+    City: 'Augusta'
+},
+{
+    State: 'Maryland',
+    City: 'Annapolis'
+},
+{
+    State: 'Massachusetts',
+    City: 'Boston'
+},
+{
+    State: 'Michigan',
+    City: 'Lansing'
+},
+{
+    State: 'Minnesota',
+    City: 'Saint Paul'
+},
+{
+    State: 'Mississippi',
+    City: 'Jackson'
+},
+{
+    State: 'Missouri',
+    City: 'Jefferson City'
+},
+{
+    State: 'Montana',
+    City: 'Helena'
+},
+{
+    State: 'Nebraska',
+    City: 'Lincoln'
+},
+{
+    State: 'Nevada',
+    City: 'Carson City'
+},
+{
+    State: 'New Hampshire',
+    City: 'Concord'
+},
+{
+    State: 'New Jersey',
+    City: 'Trenton'
+},
+{
+    State: 'New Mexico',
+    City: 'Santa Fe'
+},
+{
+    State: 'New York',
+    City: 'Albany'
+},
+{
+    State: 'North Carolina',
+    City: 'Raleigh'
+},
+{
+    State: 'North Dakota',
+    City: 'Bismarck'
+},
+{
+    State: 'Ohio',
+    City: 'Columbus'
+},
+{
+    State: 'Oklahoma',
+    City: 'Oklahoma City'
+},
+{
+    State: 'Oregon',
+    City: 'Salem'
+},
+{
+    State: 'Pennsylvania',
+    City: 'Harrisburg'
+},
+{
+    State: 'Rhode Island',
+    City: 'Providence'
+},
+{
+    State: 'South Carolina',
+    City: 'Columbia'
+},
+{
+    State: 'South Dakota',
+    City: 'Pierre'
+},
+{
+    State: 'Tennessee',
+    City: 'Nashville'
+},
+{
+    State: 'Texas',
+    City: 'Austin'
+},
+{
+    State: 'Utah',
+    City: 'Salt Lake City'
+},
+{
+    State: 'Vermont',
+    City: 'Montpelier'
+},
+{
+    State: 'Virginia',
+    City: 'Richmond'
+},
+{
+    State: 'Washington',
+    City: 'Olympia'
+},
+{
+    State: 'West Virginia',
+    City: 'Charleston'
+},
+{
+    State: 'Wisconsin',
+    City: 'Madison'
+},
+{
+    State: 'Wyoming',
+    City: 'Cheyenne'
+},
+]
+
+var output = '';
+
+function isCapitalCity(inputState, inputCity) {
+for (var i = 0; i < capitalCities.length; i++) {
+    if (capitalCities[i].City === inputCity) {
+        var State = capitalCities[i].State;
+    }
+    if (State === inputState) {
+        var output = true
+    } else {
+        var output = false
+    }
+}
+return output
+}
+isCapitalCity('Strawberry', 'mango')
